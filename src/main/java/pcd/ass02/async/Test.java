d43b1b3db398d7319c1aba77ed72a9888c45579a
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2025-05-07 12:26:21 +0200

    Add authors

--- src/main/java/pcd/ass02/async/Test.java
+++ src/main/java/pcd/ass02/async/Test.java
@@ -2,6 +2,10 @@ package pcd.ass02.async;
 
 import io.vertx.core.Vertx;
 
+/**
+ * @author Bedeschi Federica   federica.bedeschi4@studio.unibo.it
+ * @author Pracucci Filippo    filippo.pracucci@studio.unibo.it
+ */
 public class Test {
 
     public static void main(String[] args) {
--- src/main/java/pcd/ass02/reactive/Test.java
+++ src/main/java/pcd/ass02/reactive/Test.java
@@ -1,5 +1,9 @@
 package pcd.ass02.reactive;
 
+/**
+ * @author Bedeschi Federica   federica.bedeschi4@studio.unibo.it
+ * @author Pracucci Filippo    filippo.pracucci@studio.unibo.it
+ */
 public class Test {
 
     public static void main(String[] args) {
